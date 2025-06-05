ed7d1520e8e94fc7919182eaadb5602ced88be37
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-06-05 19:56:08 +0530

    my simplified logic in prepend

--- Linked_List/prepend-append.js
+++ Linked_List/prepend-append.js
@@ -1,69 +1,54 @@
-// Node class to represent each element in the linked list
-class Node {
-    constructor(value, next = null) {
-        this.value = value;
-        this.next = next;
+class Node{
+    constructor(value,next=null){
+        this.value=value;
+        this.next=next
     }
 }
-
-// LinkedList class to manage the list
-class LinkedList {
-    constructor() {
-        this.head = null; // Start of the list
-        this.size = 0;     // Number of elements in the list
+class LinkedList{
+    constructor(){
+        this.head=null;
+        this.size=0;
     }
-
-    // Check if the list is empty
-    isEmpty() {
-        return this.size === 0;
+    isEmpty(){
+        return this.size===0;
     }
-
-    // Return the size of the list
-    getSize() {
-        return this.size;
+    
+    prepend(value){
+        const node=new Node(value,this.head)
+        this.head=node;
+        this.size++
     }
-
-    // Add an element at the beginning of the list
-    prepend(value) {
-        const node = new Node(value, this.head); // new node points to current head
-        this.head = node;                        // update head to new node
-        this.size++;                             // increase size
-    }
-
-    // Add an element at the end of the list
-    append(value) {
-        const node = new Node(value);
-        if (this.isEmpty()) {
-            this.head = node; // if list is empty, new node is head
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next; // traverse to the last node
+    
+    append(value){
+        const node= new Node(value)
+        if(this.isEmpty()){
+            this.head=node
+        }else{
+            let prev=this.head
+            while(prev.next){
+                prev=prev.next
             }
-            current.next = node; // add new node at the end
+            prev.next=node;
         }
-        this.size++; // increase size
+        this.size++
     }
-
-    // Display the list values
-    display() {
-        if (this.isEmpty()) {
-            console.log("List is empty");
-            return;
+    display(){
+        if(this.isEmpty()){
+            console.log("Empty List")
+        }else{
+            let curr=this.head;
+            let listvalue="";
+            while(curr){
+                listvalue+=curr.value+"->";
+                curr=curr.next;
+            }
+            console.log(listvalue+"NULL")
         }
-        let current = this.head;
-        let values = '';
-        while (current) {
-            values += current.value + ' -> ';
-            current = current.next;
-        }
-        console.log(values + 'null');
     }
 }
-
-// Example usage
 const list = new LinkedList();
-list.append(10);
-list.append(20);
-list.prepend(5);
-list.display(); // Output: 5 -> 10 -> 20 -> null
+list.prepend(30);
+list.prepend(20);
+list.append(40);
+list.append(50);
+list.display();
