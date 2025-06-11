3d0574b27954f94dc0c5893266e4f029fee4aa2a
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-06-11 23:13:41 +0530

    QuickSort

---
+++ Sort_algorithm/QuickSort.js
@@ -0,0 +1,19 @@
+function QuickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...QuickSort(left), pivot, ...QuickSort(right)];
+}
+const arr = [4, 2, 5, 3, 1];
+console.log(QuickSort(arr));
